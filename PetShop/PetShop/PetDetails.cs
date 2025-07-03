8c522326c750ed5742c52f084900ee0eda87bb49
phaticroman <rokibulroman70@gmail.com>
2025-07-03 23:41:07 +0600

    Pet Details Table

---
+++ PetShop/PetShop/PetDetails.cs
@@ -0,0 +1,17 @@
+﻿using Azure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop
+{
+    public class PetDetails
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public int Quantity { get; set; }
+       
+    }
+}
