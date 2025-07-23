72c104ad56c95d4f3c53c3223c7471a3bc033f52
phaticroman <rokibulroman70@gmail.com>
2025-07-24 00:21:08 +0600

    Add services folder

---
+++ PetShop/PetShop/Services/BuyingRecord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Services
+{
+   public class BuyingRecord
+    {
+        public int Id { get; set; }
+        public string customerName { get; set; }
+        public string? petName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime Date { get; set; }
+        public string PetType { get; set; }
+        public bool IsAddedToStore { get; set; }
+
+    }
+}
